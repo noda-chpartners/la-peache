f85bc722bdfface69b69eb4da79c77e08dd07aae
Mode-s <shota.umemoto7@gmail.com>
2026-07-22 18:59:56 +0900

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	// OGP・canonical に使用。本番ドメインに差し替えてください
-	site: 'https://example.com',
+	site: 'https://la-peache.pages.dev',
 });
